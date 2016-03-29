75575e0a58d561b974ef5911af755eeea16d9d88
tom10987 <tom10987@gmail.com>
2016-03-29 20:21:15 +0900

    new: リソース生成、ゲーム機能の基底クラス作成

---
+++ Unity.Imagine/Assets/Scripts/AR/AbstractGame.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public abstract class AbstractGame : MonoBehaviour {
+  public abstract void Action();
+  public abstract bool IsFinish();
+  public abstract Transform GetWinner();
+
+  public string gameRule { get; protected set; }
+
+  public ARModel player1 { get; protected set; }
+  public ARModel player2 { get; protected set; }
+
+  public static void AddComponent<T>(ARDeviceManager manager)
+    where T : AbstractGame {
+    manager.player1.gameObject.AddComponent<T>();
+    manager.player2.gameObject.AddComponent<T>();
+  }
+}
