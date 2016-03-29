8ad3aba5cb91b9b2c2f399184c9a10349ee8200a
zun-ferrum <dry.tm.fe@icloud.com>
2016-03-29 15:27:51 +0900

    Title修正

--- Unity.Imagine/Assets/Scripts/Title/TitleDirecter.cs
+++ Unity.Imagine/Assets/Scripts/Title/TitleDirecter.cs
@@ -4,8 +4,6 @@ using UnityEngine.SceneManagement;
 public class TitleDirecter : MonoBehaviour
 {
 
-    const string LOAD_SCENE_NAME = "Menu";
-
     [SerializeField]
     AudioPlayer _player = null;
 
