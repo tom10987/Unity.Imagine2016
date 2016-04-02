de66bcc42645323f4d25c524ca0956a4574fed54
shunta13 <ike3shunta@yahoo.co.jp>
2016-04-03 03:30:21 +0900

    ディフェンスのゲームが一応できた。

---
+++ Unity.Imagine/Assets/Scripts/MiniGame/Pendulum/PendulumTest.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PendulumTest : ActionManager
+{
+    GameObject _shieldObj = null;
+    public GameObject shieldObj
+    {
+        get
+        {
+            if(_shieldObj == null)
+            {
+                _shieldObj = Resources.Load("MiniGame/Pendulum/Shield") as GameObject;
+            }
+            return _shieldObj;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    public GameObject CreateShield(GameObject hitParticle)
+    {
+        var shield = Instantiate(shieldObj);
+        shield.transform.position = transform.position;
+        shield.transform.rotation = transform.rotation;
+        shield.transform.Translate(new Vector3(0.0f, 0.0f, 0.8f * 40.0f));
+        shield.transform.parent = transform;
+        shield.name = shieldObj.name;
+        shield.GetComponent<Shield>().hitParticle = hitParticle;
+        _shieldObj = shield;
+        //_shield.transform.Translate(shield.transform.position);
+        //transform.LookAt(Enemy.transform);
+
+        return _shieldObj;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+	}
+
+    public override void Action(ARModel model)
+    {
+        transform.LookAt(Enemy.transform);
+        rotation = transform.eulerAngles;
+        if (_shieldObj == null)
+        {
+            if (GetComponentInChildren<Shield>() != null)
+            {
+                var shield = GetComponentInChildren<Shield>();
+                shield.enabled = true;
+                shield.Reset();
+                var obj = GetComponentInChildren<Shield>().gameObject;
+                if (obj != null) { _shieldObj = obj; }
+            }
+        }
+        if (Input.GetKeyDown(keyCode))
+        {
+            //Debug.Log(keyCode + " : ペンデュラム : " + Enemy.transform.name);
+            if (_shieldObj == null) { return; }
+            _shieldObj.GetComponent<Shield>().PushOn();
+        }
+        //if (Input.GetKey(keyCode))
+        //{
+            //transform.Rotate(5.0f, 0.0f, 0.0f);
+        //}
+    }
+}
